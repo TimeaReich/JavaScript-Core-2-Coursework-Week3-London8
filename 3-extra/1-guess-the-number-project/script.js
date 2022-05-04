6005df4bd05351d02e1f720655563727d29011b4
Timea <timea.reich@gmail.com>
2022-05-04 17:52:35 +0100

    extra done

--- 3-extra/1-guess-the-number-project/script.js
+++ 3-extra/1-guess-the-number-project/script.js
@@ -1,8 +1,43 @@
-let randomNumber = Math.floor(Math.random() * 100 + 1);
+function generateRandomNum() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+let randomNumber = generateRandomNum();
+console.log(randomNumber);
+
+let tries = 0;
 
 function guessNumber() {
-  //Collect input from the user
+  tries++;
+  console.log(tries);
+  if (tries === 7) {
+    document.querySelector(
+      ".final-output"
+    ).innerHTML = `You Lose, the number was ${randomNumber}`;
+    return;
+  }
+
   let guess = document.querySelector(".inputs-Values").value;
+  if (guess > 100 || guess < 0 || guess === "") {
+    document.querySelector(".final-output").innerHTML =
+      "Please enter a number between 1 and 100";
+  }
+
+  document.querySelector(
+    ".Tries-output"
+  ).innerHTML = `Number of tries: ${tries}`;
+
+  if (guess > randomNumber) {
+    document.querySelector(".final-output").innerHTML =
+      "Number is too high, try again";
+  } else if (guess < randomNumber) {
+    document.querySelector(".final-output").innerHTML =
+      "Number is too low, try again";
+  } else {
+    document.querySelector(
+      ".final-output"
+    ).innerHTML = `Guess is correct. You win!`;
+  }
 
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
 
@@ -18,10 +53,13 @@ function guessNumber() {
 // 1. Reset the values inside the body of the function
 // 2. Attach our new game button using an event listener to the .btnNewGame button
 function newGame() {
-  //Your code here
-  //Reset randomNumber
-  //Reset users input field
-  //Reset tries, and triesTaken by the user
+  tries = 0;
+  randomNumber = generateRandomNum();
+  console.log(tries);
+  document.querySelector(".Tries-output").innerHTML = "";
+  document.querySelector(".inputs-Values").value = "";
+  document.querySelector(".final-output").innerHTML =
+    "Guess a number between 1 and 100";
 }
 
 //keyboard exception
@@ -33,3 +71,4 @@ function keyBoardEvents(e) {
 
 document.querySelector(".btnGuess").addEventListener("click", guessNumber);
 document.addEventListener("keypress", keyBoardEvents);
+document.querySelector(".btnNewGame").addEventListener("click", newGame);
